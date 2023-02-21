069e3e70f2c197156a6b1194c7e3639bae170ff2
skmayank <msaraf07@gmail.com>
2023-02-21 15:38:44 +0530

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import List from './components/Groceries/List'
+import Checkout from './components/Groceries/Checkout';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<List />} />
+            <Route path="checkout" element={<Checkout />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
